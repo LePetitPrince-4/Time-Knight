843390acb4740a152da9c5c976a64d34937d7013
LePetitPrince-4 <lucasmleprince@gmail.com>
2025-09-23 09:54:03 -0400

    Build Version

--- Assets/KnightHandler.cs
+++ Assets/KnightHandler.cs
@@ -1,7 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class KnightHandler : MonoBehaviour
@@ -10,17 +14,35 @@ public class KnightHandler : MonoBehaviour
     public UnityEvent resetHorses;
 
     public List<ActivePlayer> players;
-
+    public bool resetRecently;
 
     public void NewDay()
     {
+        if (resetRecently)
+        {
+            return;
+        }
+
+        resetRecently = true;
         resetHorses.Invoke();
         foreach (ActivePlayer player in players)
         {
             player.NewKnight();
         }
-        
+
+        Invoke("ReadyForNextTurn", 2);
     }
 
+    public void ReadyForNextTurn()
+    {
+        resetRecently = false;
+    }
 
+    public void Update()
+    {
+        if (Input.GetKey("r"))
+        {
+            SceneManager.LoadScene("Scenes/The Field");
+        }
+    }
 }
